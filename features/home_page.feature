fd4e7f685ebe9d71709f1445e6259baf417e3c0d
aimee daniells <aimee@edendevelopment.co.uk>
2011-04-10 19:45:39 +0100

    Add phone number and mark up as a vcard.

--- features/home_page.feature
+++ features/home_page.feature
@@ -3,3 +3,4 @@ Feature: Home page
   Scenario: Some interested person visits the home page
     When I go to the home page
     Then I should see the site home page
+    And I should see contact details
--- features/step_definitions/viewing_steps.rb
+++ features/step_definitions/viewing_steps.rb
@@ -1,4 +1,8 @@
 Then /^I should see the site home page$/ do
-  page.should have_css('h1', :content => 'aimee daniells')
-  page.should have_css('h2', :content => 'software crafter')
+  page.should have_css('h1', content: 'aimee daniells')
+  page.should have_css('h2', content: 'software crafter')
+end
+
+Then /^I should see contact details$/ do
+  page.should have_css('.vcard .tel .value', content: '074 032 68 398')
 end
